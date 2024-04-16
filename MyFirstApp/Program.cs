6d1550ca5d9972e5d848a28535432825290c94ef
Morris Vaide <mvaide@tktk.ee>
2024-04-16 16:46:15 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿//This is a comment to my code
+
+//rakendus
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+string username; 
+
+username = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + username);
+
